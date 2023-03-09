32f4fabf1105306667783cb1525b672b0e23b1db
Jason Hsieh <Jason.CY.Hsieh@ibm.com>
2023-03-09 19:08:15 +0800

    add: initializer

--- credit_card.rb
+++ credit_card.rb
@@ -9,12 +9,20 @@ class CreditCard
 
   def initialize(number, expiration_date, owner, credit_network)
     # TODO: initialize the instance variables listed above
+    @number = number
+    @expiration_date = expiration_date
+    @owner = owner
+    @credit_network = credit_network
   end
 
   # returns json string
   def to_json
     {
       # TODO: setup the hash with all instance vairables to serialize into json
+      'number' => @number,
+      'expiration_date' => @expiration_date,
+      'owner' => @owner,
+      'credit_network' => @credit_network,
     }.to_json
   end
 
