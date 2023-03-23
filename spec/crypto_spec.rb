b07f621084548d0dbcce409f22b32202f966f76b
Jason Hsieh <Jason.CY.Hsieh@ibm.com>
2023-03-23 22:44:34 +0800

    add: test for sk cipher

--- spec/crypto_spec.rb
+++ spec/crypto_spec.rb
@@ -3,6 +3,7 @@
 require_relative '../credit_card'
 require_relative '../substitution_cipher'
 require_relative '../double_trans_cipher.rb'
+require_relative '../sk_cipher.rb'
 require 'minitest/autorun'
 
 describe 'Test card info encryption' do
@@ -10,6 +11,7 @@ describe 'Test card info encryption' do
     @cc = CreditCard.new('4916603231464963', 'Mar-30-2020',
                          'Soumya Ray', 'Visa')
     @key = 3
+    @sk_key = ModernSymmetricCipher.generate_new_key()
   end
 
   describe 'Using Caesar cipher' do
@@ -52,6 +54,20 @@ describe 'Test card info encryption' do
     it 'should decrypt text' do
       enc = DoubleTranspositionCipher.encrypt(@cc, @key)
       dec = DoubleTranspositionCipher.decrypt(enc, @key)
+      _(dec).must_equal @cc.to_s
+    end
+
+  end
+    describe 'Using Modern Symmetric Cipher' do
+    it 'should encrypt card information' do
+      enc = ModernSymmetricCipher.encrypt(@cc, @sk_key)
+      _(enc).wont_equal @cc.to_s
+      _(enc).wont_be_nil
+    end
+
+    it 'should decrypt text' do
+      enc = ModernSymmetricCipher.encrypt(@cc, @sk_key)
+      dec = ModernSymmetricCipher.decrypt(enc, @sk_key)
       _(dec).must_equal @cc.to_s
     end
   end
